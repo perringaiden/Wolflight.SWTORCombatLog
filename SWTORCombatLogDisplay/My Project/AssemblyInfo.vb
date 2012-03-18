6c5379f0304f4be95f709a148ff36ecbf8da793d
Brent Hollett <perrin@techie.com>
2012-03-18 13:31:03 +1000

    Stored new compiled version after critical value fix

--- SWTORCombatLogDisplay/My Project/AssemblyInfo.vb
+++ SWTORCombatLogDisplay/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.0.1")> 
-<Assembly: AssemblyFileVersion("0.0.0.1")> 
+<Assembly: AssemblyVersion("0.0.0.2")> 
+<Assembly: AssemblyFileVersion("0.0.0.2")> 
